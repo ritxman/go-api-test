d6e2ac429c83b24be7711d0e80cd39af12b3088b
martinprakasa <martin.prakasa@bukalapak.com>
2024-12-14 17:30:22 +0700

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module learning
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	r := mux.NewRouter()
+
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Get all environment variables
+	envVars := os.Getenv("PORT")
+
 	r.HandleFunc("/api/test/{value}", test)
-	fmt.Println("Server is running on :8080")
-	http.ListenAndServe(":8080", r)
+	fmt.Println("Server is running on :", envVars)
+	http.ListenAndServe(":"+envVars, r)
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request received")
 	vars := mux.Vars(r)
 	value := vars["value"]
 
